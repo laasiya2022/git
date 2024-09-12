cbc4ee8e3b1395da90b3b8cc45d556d7f155886f
day <laasiya.sivanadam@gmail.com>
2024-09-12 10:21:41 +0530

    new code added

---
+++ src/main/java/Sample1.java
@@ -0,0 +1,17 @@
+
+public class Sample1 {
+	private void empId() {
+		System.out.println("Employee id is 767");
+
+	}
+	private void empName() {
+		System.out.println("Employee name is lasi");
+
+	}
+	private void empAddress() {
+		System.out.println("Employee Adress is thoraipakkam");
+		
+
+	}
+
+}
